4149280dad687ffbaa08dd92f418b418d51f4ad6
Shanks <kannanshankar130203@gmail.com>
2026-03-31 21:45:36 +0530

    neetcode 150 start

---
+++ NC_001_ContainsDuplicates.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+bool containsDuplicate(vector<int> &nums){
+  unordered_map<int, int> hashMap;
+  for (int i = 0; i < nums.size(); i++){
+    // cout << i <<" added to count of hashMap"<<endl;
+    hashMap[nums[i]]++;
+  }
+  for (auto i : hashMap){
+    // cout << "count of " << i.first << " is " << i.second<<endl;
+    if (i.second > 1){
+      return true;
+    }
+  }
+  return false;
+}
