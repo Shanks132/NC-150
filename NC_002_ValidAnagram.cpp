06bdb99babb3e665ece43f6399207cbc44510498
Shanks <kannanshankar130203@gmail.com>
2026-04-01 23:03:11 +0530

    Problem no.2

---
+++ NC_002_ValidAnagram.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+bool isAnagram(string s, string t)
+{
+  unordered_map<char, int> s_letters;
+  unordered_map<char, int> t_letters;
+  for (auto ch : s)
+  {
+    s_letters[ch]++;
+  }
+
+  for (auto ch : t)
+  {
+    s_letters[ch]--;
+  }
+  for (auto letter : s_letters)
+  {
+    if (letter.second != 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
